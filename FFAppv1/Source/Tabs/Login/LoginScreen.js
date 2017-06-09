caca14a94f1bb1257bbb06c5ee568e9422d629d4
Frank Antolino <fantolino17@gmail.com>
2017-06-09 14:40:30 -0400

    Added tab for login

---
+++ FFAppv1/Source/Tabs/Login/LoginScreen.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+import LoginForm from '../../Components/LoginForm'
+
+class LoginScreen extends Component {
+
+  render(){
+    return (
+      <LoginForm />
+    )
+  }
+}
+
+export default LoginScreen
