d3898cf3eba97621482245121dd3f05f0033e0b2
Frank Antolino <fantolino17@gmail.com>
2017-06-09 14:39:05 -0400

    Added AuthReducer, JobReducer, Updated index

---
+++ FFAppv1/Source/Reducers/JobFormReducer.js
@@ -0,0 +1,38 @@
+import {JOB_UPDATE, JOB_CREATE, JOBS_FETCH_SUCCESS} from '../Actions/types'
+
+const INITIAL_STATE = {
+  name: '',
+  email: '',
+  category: ''
+}
+
+export default (state = INITIAL_STATE, action) => {
+  switch(action.type){
+    case JOB_UPDATE:
+      return {...state, [action.payload.prop]: action.payload.value}
+    case JOB_CREATE:
+      return {INITIAL_STATE}
+    case JOBS_FETCH_SUCCESS:
+      console.log("job fetch successful!!")
+      console.log(action.payload)
+      return action.payload
+    default:
+      return state
+  }
+}
+
+
+// import {JOBS_FETCH_SUCCESS} from '../Actions/types'
+
+// const INITIAL_STATE = {}
+
+// export default (state = INITIAL_STATE, action) => {
+//     switch(action.type){
+//       case JOBS_FETCH_SUCCESS:
+//         console.log("job fetch successful!!")
+//         console.log(action.payload)
+//         return action.payload
+//       default: 
+//         return state
+//   }
+// }
--- FFAppv1/Source/Reducers/index.js
+++ FFAppv1/Source/Reducers/index.js
@@ -2,12 +2,16 @@ import {combineReducers} from 'redux';
 import LibraryReducer from './LibraryReducer';
 import SelectionReducer from './SelectionReducer';
 import JobReducer from './JobReducer';
+import JobFormReducer from './JobFormReducer'
+import AuthReducer from './AuthReducer'
 
 export default combineReducers({
 	libraries: LibraryReducer,
 	selectedLibraryId: SelectionReducer,
 	jobs: JobReducer,
-	selectedJobId: SelectionReducer
+	selectedJobId: SelectionReducer,
+	auth: AuthReducer,
+	jobForm: JobFormReducer 
 });
 
 //console.log(store.getState());
