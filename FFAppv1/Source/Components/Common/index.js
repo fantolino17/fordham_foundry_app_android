fdb3d2478397c3c040c0c33f04cc13b7e09440bd
Frank Antolino <fantolino17@gmail.com>
2017-06-11 10:21:33 -0400

    Create index.js

--- FFAppv1/Source/Components/Common/index.js
+++ FFAppv1/Source/Components/Common/index.js
@@ -4,5 +4,5 @@ export * from './CardSection'
 export * from './Header'
 export * from './Input'
 export * from './Spinner'
-//export * from './Confirm'
+export * from './Confirm'
 //Can import folder, will import this file bc named index
