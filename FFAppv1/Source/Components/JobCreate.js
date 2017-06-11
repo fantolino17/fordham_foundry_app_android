dc012beeadc7ef372e0f3096521f163c7b3e9cfb
Frank Antolino <fantolino17@gmail.com>
2017-06-11 14:57:55 -0400

    Add files via upload

--- FFAppv1/Source/Components/JobCreate.js
+++ FFAppv1/Source/Components/JobCreate.js
@@ -1,20 +1,42 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
+import {Text} from 'react-native'
+import firebase from 'firebase'
 import {jobUpdate, jobCreate, fetchJobs} from '../Actions'
-import {Card, CardSection, Button} from './Common'
+import {Card, CardSection, Button, Confirm} from './Common'
 import JobForm from './JobForm'
 
 class JobCreate extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      showModal: false
+    }
+  }
+
+  renderModal(){
+    this.setState({showModal: true})
+  }
+  onReturn(){
+    this.setState({showModal: false})
+  }
 
   onButtonPress(){
-    const {name, email, description, category} = this.props
-
-    this.props.jobCreate({
-      name: name || 'No Name Provided', 
-      email: email || 'No Email Provided', 
-      description: description || 'No Description Provided', 
-      category: category || 'Programmer'})
-  }
+    const {name, email, description, category, user, loggedIn} = this.props
+    
+    if(loggedIn){
+      const {currentUser} = firebase.auth()
+      this.props.jobCreate({
+        name: name || 'No Name Provided', 
+        email: email || 'No Email Provided', 
+        description: description || 'No Description Provided', 
+        category: category || 'Programmer',
+        user: currentUser.uid || null    
+      })
+    }else{
+      this.setState({showModal: true})
+    }
+}
 
   display(){
     this.props.fetchJobs({category: 'marketing'})
@@ -24,6 +46,16 @@ class JobCreate extends Component {
   render(){
     return (
       <Card>
+        
+        <Confirm
+          visible={this.state.showModal}
+          onReturn={this.onReturn.bind(this)}
+			  >
+          <Text style={styles.modalTextStyle}>
+            Oops! Ya need to log in to post!
+          </Text>
+				</Confirm>
+
         <JobForm {...this.props}/>
         <CardSection>
           <Button onPress={this.onButtonPress.bind(this)}>
@@ -42,9 +74,18 @@ class JobCreate extends Component {
   }
 }
 
+const styles = {
+  modalTextStyle:{
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: 24
+  }
+}
+
 const mapStateToProps = (state) => {
   const {name, email, description, category} = state.jobForm
-  return {name, email, description, category} 
+  const {user, loggedIn} = state.auth
+  return {name, email, description, category, user, loggedIn} 
 }
 
 export default connect(mapStateToProps,{jobUpdate, jobCreate, fetchJobs})(JobCreate)
