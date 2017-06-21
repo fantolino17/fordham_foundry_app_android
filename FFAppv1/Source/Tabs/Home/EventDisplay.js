6c02cfe1cbe57928a624fbe5d0b2011fadf0084c
Frank Antolino <fantolino17@gmail.com>
2017-06-21 15:01:20 -0400

    Add files via upload

--- FFAppv1/Source/Tabs/Home/EventDisplay.js
+++ FFAppv1/Source/Tabs/Home/EventDisplay.js
@@ -1,18 +1,26 @@
 import React from 'react'
-import {Text, View} from 'react-native'
+import {Text, View, TouchableOpacity} from 'react-native'
 import {connect} from 'react-redux';
 import {Card, CardSection, Button, Confirm} from '../../Components/Common'
 
 
-const EventDisplay = ({event}) => {
-
+const EventDisplay = ({title, location, date, time}) => {
+console.log(title)
   return (
     <Card>
-      <CardSection>
-        <Text style={}>Event:</Text>
-        <Text> {event.title}</Text>
-      </CardSection>
+        <View>
+          {title!==null ? <Text>{title}</Text> : <Text>The title is not specified</Text>}
+          {location!==null ? <Text>{location}</Text> : <Text>The location is not specified</Text>}
+          {date!==null ? <Text>{date}</Text> : <Text>The date is not specified</Text>}
+          {time!==null ? <Text>{time}</Text> : <Text>The time is not specified</Text>}
+        </View>
+        
+        <Button>
+          Set Reminder!
+        </Button>
 
     </Card>
   )
 }
+
+export { EventDisplay }
