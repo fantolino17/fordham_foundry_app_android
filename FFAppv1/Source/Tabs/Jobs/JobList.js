55ddf3df63e64e1b9349ece27d952cb102be3415
Frank Antolino <fantolino17@gmail.com>
2017-06-22 14:24:19 -0400

    Add files via upload

--- FFAppv1/Source/Tabs/Jobs/JobList.js
+++ FFAppv1/Source/Tabs/Jobs/JobList.js
@@ -1,17 +1,17 @@
 import React, {Component} from 'react';
-import {Text, ListView, StyleSheet, View, ScrollView, TouchableOpacity} from 'react-native';
+import {Text, ListView, StyleSheet, View, ScrollView, TouchableOpacity, TouchableWithoutFeedback} from 'react-native';
 import {connect} from 'react-redux';
 import {fetchJobs, jobDelete} from '../../Actions';
 import JobForm from './JobForm';
 import firebase from 'firebase';
 
 //import * as actions from '../Actions';
-
 import JobListItem from './JobListItem';
 import JobSection from './JobSection';
-import JobCreate from './JobCreate'
-import {JobDisplay} from './JobDisplay'
-import {Card, CardSection, Button, Confirm} from '../../Components/Common'
+import JobCreate from './JobCreate';
+import {JobDisplay} from './JobDisplay';
+import {Card, CardSection, Button, ButtonCont, Confirm, CSCol} from '../../Components/Common';
+import ViewContainer from '../../Components/Common/ViewContainer';
 
 class JobList extends Component {
 	constructor(props) {
@@ -26,13 +26,7 @@ class JobList extends Component {
 	}
 
 	componentWillMount(){
-		console.log(this.props.jb)
-		this.props.fetchJobs()
-	}
-
-	componentDidMount(){
-		this.props.fetchJobs()
-		console.log(this.props.jb)
+		this.props.fetchJobs()		
 	}
 
 	renderModal(){
@@ -58,6 +52,7 @@ class JobList extends Component {
 	}
 
 	fetchJobBoard(){
+
 		jb = this.props.jb
 		rows=[]
 		if(this.props.user === null){
@@ -67,62 +62,65 @@ class JobList extends Component {
 		}
 		for(var key in jb){
 			const temp = key
-
+			//changed from if to else if
 			if(currentUserId === jb[key].user){ //If current user created job, show delete button
 				rows.unshift(
-				<TouchableOpacity onPress={ ()=>{this.renderDes(temp,true)} }>
-					<CardSection>
-							<Text style={styles.userJobTextStyle}>Business Name:</Text>
-							<Text> {jb[key].name}</Text>
-							<Text> ({jb[key].title})</Text>
-					</CardSection>
+				<TouchableOpacity onPress={() => {this.renderDes(temp,true)}}>
+					<CSCol>
+							<Text style={styles.userJobTextStyle}>{jb[key].category}</Text>
+							<Text style={styles.nameStyle}>{jb[key].name}</Text>
+							<Text style = {styles.dateStyle}>Date</Text>
+					</CSCol>
+
 				</TouchableOpacity>
 				)
 			}else{ //If user did not create this job, dont show delete button (2nd param in renderDes)
 				rows.unshift(
 				<TouchableOpacity onPress={ ()=>{this.renderDes(temp,false)} }>
-					<CardSection>
-							<Text style={styles.textStyle}>Business Name:</Text>
-							<Text> {jb[key].name}</Text>
-							<Text> ({jb[key].title})</Text>
-					</CardSection>
+					<CSCol>
+							<Text style={styles.catStyle}>{jb[key].category}</Text>
+							<Text style = {styles.nameStyle}>{jb[key].name}</Text>
+							<Text style = {styles.dateStyle}>Date</Text>
+					</CSCol>
 				</TouchableOpacity>
 				)
 			}
 		}
-		return <ScrollView removeClippedSubviews={false}>{rows}</ScrollView>
+		return <ScrollView>{rows}</ScrollView>
 	}
 
 
 	render() {
-		console.log(this.props.jb)
-		console.log("RENDERING")
+
 		return(
-		
-			<Card>
-				<Confirm
-          visible={this.state.showModal}
-          onReturn={this.onReturn.bind(this)}
-				>
-					<JobCreate style={{flex:1}}/>
-				</Confirm>
-
-				<Confirm 
-					visible={this.state.showDesModal}
-					onReturn={this.onReturn.bind(this)}
-				>
-					<JobDisplay jobBoard={this.props.jb} jobKey={this.state.curKey} canDelete={this.state.canDelete} jobDelete={this.props.jobDelete} />
-				</Confirm>
-
-				<CardSection>	
+			<View>
+				<ButtonCont>
 					<Button onPress={this.renderModal.bind(this)}>
-						Post to Job Board!
+					Post to the Job Board!
 					</Button>
-				</CardSection>
-								
-				{this.fetchJobBoard()}
+				</ButtonCont>
 
-			</Card>
+			{this.fetchJobBoard()}
+			
+			<Confirm
+        visible={this.state.showModal}
+        onReturn={this.onReturn.bind(this)}
+			>
+			
+				<JobCreate style={{flex:1}}/>
+			
+			</Confirm>
+
+			<Confirm 
+				visible={this.state.showDesModal}
+				onReturn={this.onReturn.bind(this)}
+			>
+					<JobDisplay jobBoard={this.props.jb} jobKey={this.state.curKey} canDelete={this.state.canDelete} jobDelete={this.props.jobDelete} />
+			</Confirm>
+			
+
+			</View>
+
 		
 		);
 	}
@@ -130,11 +128,34 @@ class JobList extends Component {
 
 const styles = StyleSheet.create({
 	container: {
-		flex: 1
+		padding: 8
 	},
  	separator: {
     height: StyleSheet.hairlineWidth,
     backgroundColor: '#8E8E8E',
+  },
+  catStyle: {
+  	fontSize: 16,
+  	paddingLeft: 8,
+  	color:'blue',
+	fontWeight: 'bold',
+	fontFamily: "GillSans"
+  },
+  busStyle: {
+  	fontSize: 16,
+  	paddingLeft: 8,
+	fontFamily: "GillSans"
+  },
+  dateStyle: {
+  	paddingTop: 6,
+  	paddingLeft: 8,
+  	fontSize: 14,
+  	fontFamily: "GillSans"
+  },
+  nameStyle: {
+  	fontSize: 16,
+  	paddingLeft: 8,
+  	fontFamily: "GillSans"
   },
 	textStyle: {
 		fontSize: 16,
@@ -142,7 +163,10 @@ const styles = StyleSheet.create({
 	},
 	userJobTextStyle:{
 		fontSize: 16,
-		fontStyle: 'italic'
+		paddingLeft: 8,
+		color: 'darkgreen',
+		fontWeight: 'bold',
+		fontFamily: "GillSans"
 	}
 });
 
